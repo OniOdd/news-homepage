9ea0eba028a6b878b248bc6957daf4f0579bede3
Mykyta Kavetskyi <103828218+OniOdd@users.noreply.github.com>
2026-02-19 21:17:09 +0200

    Add burger menu logic to main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const burgerMenuBtn = document.getElementById("js-burger-menu");
+
+burgerMenuBtn.addEventListener("click", () => {
+  const bodyElem = document.body;
+  const navMenuElem = document.getElementById("js-menu");
+
+  bodyElem.classList.toggle("lock-screen");
+  navMenuElem.classList.toggle("show-menu");
+  burgerMenuBtn.classList.toggle("close-menu");
+});
